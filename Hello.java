d35c38af6e78d2656285f508a1e88d337c24a8d0
Ayomidefs <69921252+Ayomidefs@users.noreply.github.com>
2020-08-19 14:50:03 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/****************************************
+*Hello.java
+******************************************
+*Ayomide Fakulujo
+*August 19, 2020
+*CMSC 255-002
+*****************************************/
+public class Hello {
+	public static void main(String[]args) {
+	System.out.println("Ayomide Fakulujo");
+	System.out.println("Lab 1");
+	System.out.println("Introduction to java");
+	System.out.println("August 19, 2020");
+	System.out.println("CMSC 255-002");
+	System.out.println();
+	}
+}
